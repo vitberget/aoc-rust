94d673d127141337e346779717afb3cf487d6250
Kenneth Hedman <github@vitberget.se>
2024-08-26 11:44:21 +0200

    2018 7 p2

--- src/bin/year2018_day7/main.rs
+++ src/bin/year2018_day7/main.rs
@@ -1,12 +1,11 @@
 use std::collections::{HashMap, HashSet};
-use std::fs::read_to_string;
 use std::env::args;
+use std::fs::read_to_string;
 
 use aoc_utils::get_aoc_filename;
 use itertools::Itertools;
 use lazy_static::lazy_static;
 use regex::Regex;
-
 
 pub fn main() -> anyhow::Result<()> {
     let filename = get_aoc_filename(args(), 2018, 7);
@@ -16,12 +15,11 @@ pub fn main() -> anyhow::Result<()> {
     let p1 = solve_part_1(text)?;
     println!("Part 1: {p1}");
 
-    // let p2 = solve_part_2(text)?;
-    // println!("Part 2: {p2}");
+    let p2 = solve_part_2(text, 60, 5)?;
+    println!("Part 2: {p2}");
 
     Ok(())
 }
-
 
 fn solve_part_1(text: &str) -> anyhow::Result<String> {
     let mut deps: HashMap<char, HashSet<char>> = HashMap::new();
@@ -52,6 +50,57 @@ fn solve_part_1(text: &str) -> anyhow::Result<String> {
     Ok(answer)
 }
 
+fn solve_part_2(text: &str, time_offset: usize, num_robots: usize) -> anyhow::Result<usize> {
+    let mut deps: HashMap<char, HashSet<char>> = HashMap::new();
+    for (l2, l1) in text.lines().map(line_to_dep) {
+        deps.entry(l1)
+            .and_modify(|set| { set.insert(l2); })
+            .or_insert(HashSet::from([l2]));
+
+        deps.entry(l2).or_default();
+    }
+
+    let mut answer: String = String::new();
+    let mut now: usize = 0;
+    let mut robots: Vec<(char, usize)> = vec![];
+
+    while ! deps.is_empty() {
+        let complete_robots = robots.iter().filter(|(_, time)| time <= &now).sorted_by(|(l1,_),(l2,_)| l1.cmp(l2));
+        for (letter,_) in complete_robots {
+            answer.push(*letter);
+            deps.remove(letter);
+            deps.iter_mut().for_each(|(_,v)| { v.remove(letter); });
+        }
+
+        robots.retain(|(_,time)| time > &now);
+
+        while robots.len() < num_robots {
+            let robot_letters: HashSet<&char> = robots.iter().map(|(letter,_)| letter).collect();
+
+            let next_letter = deps.iter()
+                .filter(|(k,_)| ! robot_letters.contains(k))
+                .filter(|(_,v)| v.is_empty())
+                .map(|(k,_)| k)
+                .sorted()
+                .next();
+
+            if let Some(next_letter) = next_letter {
+                robots.push((next_letter.to_owned(), now + processing_time(*next_letter, time_offset)));
+            } else {
+                break;
+            }
+        }
+
+        now += 1;
+    }
+
+    Ok(now - 1)
+}
+
+fn processing_time(next_letter: char, time_offset: usize) -> usize {
+    next_letter as usize - 'A' as usize +  time_offset + 1
+}
+
 lazy_static! {
     static ref REGEX: Regex = Regex::new("Step ([A-Z]) must be finished before step ([A-Z]) can begin.*").unwrap();
 }
@@ -69,12 +118,24 @@ mod tests {
 
     #[test]
     fn test_parse_dep() {
-       assert_eq!(line_to_dep("Step C must be finished before step A can begin."), ('C', 'A'));
+        assert_eq!(line_to_dep("Step C must be finished before step A can begin."), ('C', 'A'));
+    }
+
+    #[test]
+    fn test_offset() {
+        assert_eq!(processing_time('A', 60), 61);
+        assert_eq!(processing_time('Z', 60), 86);
     }
 
     #[test]
     fn test_part_1() {
         let text = include_str!("example.txt");
         assert_eq!(solve_part_1(text).unwrap(), "CABDFE");
+    }
+
+    #[test]
+    fn test_part_2() {
+        let text = include_str!("example.txt");
+        assert_eq!(solve_part_2(text, 0, 2).unwrap(), 15); 
     }
 }
