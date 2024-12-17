312c44bb4973a3e1dd340504153ad8641651e5f2
Kenneth Hedman <github@vitberget.se>
2024-12-17 09:36:50 +0100

    macro aoc_puzzle!() to get puzzle file instead of get_aoc_filename(...)

--- aoc-utils/src/lib.rs
+++ aoc-utils/src/lib.rs
@@ -6,3 +6,15 @@ pub fn get_aoc_filename(args: Args, year: u16, day: u16) -> String {
         .nth(1)
         .unwrap_or(format!("puzzles/year{year}_day{day}.txt"))
 }
+
+#[macro_export]
+/// Use first argument as file if provided, otherwise use filename based on module_path
+macro_rules! aoc_puzzle {
+    () => {{
+        let __file_name = std::env::args().into_iter()
+            .nth(1)
+            .unwrap_or(format!("puzzles/{}.txt", module_path!()));
+        std::fs::read_to_string(__file_name)
+    }};
+}
+
--- aoc/src/bin/year2024_day11/main.rs
+++ aoc/src/bin/year2024_day11/main.rs
@@ -1,14 +1,10 @@
-use std::fs::read_to_string;
-use std::env::args;
-
-use aoc_utils::get_aoc_filename;
+use aoc_utils::aoc_puzzle;
 
 mod part1;
 mod part2;
 
 pub fn main() -> anyhow::Result<()> {
-    let filename = get_aoc_filename(args(), 2024, 11);
-    let text = read_to_string(filename)?;
+    let text = aoc_puzzle!()?;
     let text = text.trim();
 
     println!("Part 1: {}", part1::blink_many_times(text, 25));
@@ -16,4 +12,3 @@ pub fn main() -> anyhow::Result<()> {
 
     Ok(())
 }
-
