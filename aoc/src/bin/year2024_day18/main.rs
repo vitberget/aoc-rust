883d7e7f6c1896412dfc2d5ae1c7dfb6c8d1e6ce
Kenneth Hedman <github@vitberget.se>
2024-12-26 19:54:04 +0100

    AOC_PRINTING

--- aoc/src/bin/year2024_day18/main.rs
+++ aoc/src/bin/year2024_day18/main.rs
@@ -7,7 +7,7 @@ pub fn main() -> anyhow::Result<()> {
     let puzzle_text = aoc_puzzle!()?;
     let puzzle_text = puzzle_text.trim();
 
-    let print_it = true;
+    let print_it = std::env::var("AOC_PRINTING").is_ok();
 
     println!("Part 1: {}", part1::part1(puzzle_text, 1024, 70, 70, print_it, 100)?.0);
     println!("Part 2: {:?}", part2::part2(puzzle_text, 1024, 70, 70, print_it)?);
