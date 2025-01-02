04bbd2737688099164ab26124516912c51770b77
Kenneth Hedman <github@vitberget.se>
2025-01-02 22:21:22 +0100

    year2016 day3

---
+++ aoc/src/bin/year2016_day03/part2.rs
@@ -0,0 +1,46 @@
+use std::str::Lines;
+
+use aoc_procmacros::aoc_profile;
+use itertools::Itertools;
+use itertools::Chunk;
+
+#[aoc_profile]
+pub fn part2(text: &str) -> anyhow::Result<usize> {
+    Ok(
+        text.trim()
+        .lines()
+        .chunks(3)
+        .into_iter()
+        .map(|chunk| count_triangles(chunk))
+        .sum())
+}
+
+fn count_triangles(chunk: Chunk<Lines>) -> usize {
+    let mut iter = chunk
+        .map(|line| line.split_whitespace()
+            .flat_map(|word| word.parse::<usize>()));
+
+    let mut triangles: usize = 0;
+
+    if let Some(mut nums1) = iter.next() {
+        if let Some(mut nums2) = iter.next() {
+            if let Some(mut nums3) = iter.next() {
+                for _ in 0..3 {
+                    if let Some(n1) = nums1.next() {
+                        if let Some(n2) = nums2.next() {
+                            if let Some(n3) = nums3.next() {
+                                if n1 + n2 > n3  &&
+                                    n1 + n3 > n2 &&
+                                        n2 + n3 > n1 {
+                                            triangles += 1;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    triangles 
+}
