9c2bea19aee9a35cc79b89329b0b6b8704ba29a9
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-12-03 11:08:06 +0100

    year2025 day3 part2 wip

--- aoc/src/bin/year2025_day3/part2.rs
+++ aoc/src/bin/year2025_day3/part2.rs
@@ -2,5 +2,72 @@ use aoc_procmacros::aoc_profile;
 
 #[aoc_profile]
 pub fn part2(text: &str) -> anyhow::Result<usize> {
-    Ok(0)
+    let result = text.lines()
+        .map(|line| line.trim())
+        .map(|line| get_joltage_with_friction(line))
+        .map(|joltage| joltage.unwrap())
+        .sum();
+
+    Ok(result)
+}
+
+fn get_joltage_with_friction(line: &str) -> anyhow::Result<usize> {
+    let digits: Vec<usize> = line
+        .chars()
+        .map(|ch| ch.to_digit(10))
+        .map(|option| option.unwrap())
+        .map(|digit| digit as usize)
+        .collect();
+
+    todo!("implement")
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() -> anyhow::Result<()> {
+        let example = include_str!("example.txt");
+        let result = part1(example)?;
+        assert_eq!(result, 357);
+        Ok(())
+    }
+
+
+    // In 987654321111111, the largest joltage can be found by turning on everything except some 
+    // 1s at the end to produce 987654321111.
+    #[test]
+    fn test_joltage_1() -> anyhow::Result<()> {
+        let result = get_joltage_with_friction("987654321111111")?;
+        assert_eq!(result, 987654321111);
+        Ok(())
+    }
+
+    // In the digit sequence 811111111111119, the largest joltage can be found by turning on 
+    // everything except some 1s, producing 811111111119.
+    #[test]
+    fn test_joltage_2() -> anyhow::Result<()> {
+        let result = get_joltage_with_friction("811111111111119")?;
+        assert_eq!(result, 811111111119);
+        Ok(())
+    }
+
+    // In 234234234234278, the largest joltage can be found by turning on everything except 
+    // a 2 battery, a 3 battery, and another 2 battery near the start to produce 434234234278.
+    #[test]
+    fn test_joltage_3() -> anyhow::Result<()> {
+        let result = get_joltage_with_friction("234234234234278")?;
+        assert_eq!(result, 434234234278);
+        Ok(())
+    }
+
+    // In 818181911112111, the joltage 888911112111 is produced by turning on everything except 
+    // some 1s near the front.
+    #[test]
+    fn test_joltage_4() -> anyhow::Result<()> {
+        let result = get_joltage_with_friction("818181911112111")?;
+        assert_eq!(result, 888911112111);
+        Ok(())
+    }
 }
