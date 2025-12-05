e3dddd90545891a89cbc44295cc9e968e55a764a
Kenneth Hedman <kenneth.hedman@firefly.se>
2025-12-05 13:47:57 +0100

    year2025 day5 part1

---
+++ aoc/src/bin/year2025_day5/part1.rs
@@ -0,0 +1,43 @@
+use std::ops::RangeInclusive;
+
+use aoc_procmacros::aoc_profile;
+
+#[aoc_profile]
+pub fn part1(text: &str) -> anyhow::Result<usize> {
+    let mut ranges: Vec<RangeInclusive<usize>> = vec![];
+    let mut ingridients: Vec<usize> = vec![];
+
+    for line in text.lines() {
+        if line.contains("-") {
+            if let Some((start, end)) = line.split_once('-') {
+                let start:usize = start.parse()?;
+                let end:usize = end.parse()?;
+
+                ranges.push(start..=end);
+            }
+        } else if ! line.is_empty() {
+            ingridients.push(line.parse()?);
+        }
+    }
+
+    Ok(ingridients.iter()
+        .filter(|id| contains_ingridient(id, &ranges))
+        .count())
+}
+
+fn contains_ingridient(ingridient: &usize, ranges: &Vec<RangeInclusive<usize>>) -> bool {
+    ranges.iter().any(|range| range.contains(ingridient)) 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_example() -> anyhow::Result<()> {
+        const EXAMPLE: &str = include_str!("example.txt");
+        let result = part1(EXAMPLE)?;
+        assert_eq!(result, 3);
+        Ok(())
+    }
+}
