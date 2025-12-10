8391c14940619c51858a7a3ae7e0e86aa682b5c8
Kenneth Hedman <github@vitberget.se>
2025-12-10 16:03:10 +0100

    year2025 day8 part1

---
+++ aoc/src/bin/year2025_day8/main.rs
@@ -0,0 +1,13 @@
+use aoc_utils::aoc_puzzle;
+
+pub mod part1;
+pub mod part2;
+
+pub fn main() -> anyhow::Result<()> {
+    let text = aoc_puzzle!()?;
+
+    println!("Part 1: {}", part1::part1(&text, 1000)?);
+    println!("Part 2: {}", part2::part2(&text)?);
+
+    Ok(())
+}
